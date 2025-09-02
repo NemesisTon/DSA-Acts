7046363c4ad542bf045813088e4fe52d00f95aca
NemesisTon <tonflores99@gmail.com>
2025-09-03 07:47:05 +0800

    ikaw nang tahanan nang mundo

--- Linked_List/Variation2.c
+++ Linked_List/Variation2.c
@@ -19,7 +19,7 @@ void insertLast(List *list, int data);
 void insertPos(List *list, int data, int index);
 void deleteStart(List *list);
 void deleteLast(List *list);
-// void deletePos(List *list, int index);
+void deletePos(List *list, int index);
 // int retrieve(List *list, int index);
 // int locate(List *list, int data);
 // void insertSort(List *list, int data);
@@ -30,17 +30,29 @@ int main(){
     List *L;
 
     L = initialize();
-    insertFirst(L, 11);
-    insertFirst(L, 22);
+    insertFirst(L, 44);
     insertFirst(L, 33);
-    // insertPos(L, 55, 2);
+    insertFirst(L, 22);
+    insertFirst(L, 11);
     // insertLast(L, 99);
     // empty(L);
     // deleteStart(L);
-    deleteLast(L);
+    // deleteLast(L);
+    deletePos(L, 5);
     display(L);
 
     return 0;
+}
+void deletePos(List *list, int index){
+    if(index > list->count){
+        printf("The index should be less than or equal to count.\n");
+    }else if(index == 0){
+        deleteStart(list);
+    }else if(index == list->count){
+        deleteLast(list);
+    }else{
+        Node *trav;
+    }
 }
 
 void deleteLast(List *list){
@@ -78,7 +90,9 @@ void empty(List *list){
 }
 
 void insertPos(List *list, int data, int index){
-    if(list->head == NULL){
+    if(index > list->count){
+        printf("Invalid index it should be less than or equal to count.\n");
+    }else if(list->head == NULL || index == 0){
         insertFirst(list, data);
     }else if(list->count == index){
         insertLast(list, data);
@@ -87,13 +101,17 @@ void insertPos(List *list, int data, int index){
 
         Node *newNode = (Node *)malloc(sizeof(Node));
         newNode->data = data;
-
-        for(trav = list->head; list->count < index - 1; trav = trav->next){}
+        
+        trav = list->head;
+        for(int i = 0; i < index - 1; i++){
+            trav = trav->next;
+        }
         prev = trav->next;
         trav->next = newNode;
         newNode->next = prev;
         list->count++;
     }
+    
 }
 
 void insertLast(List *list, int data){
