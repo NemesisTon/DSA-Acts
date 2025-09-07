7d30db2f2e42f90fa8f622cdb0fad4e58f2bb087
NemesisTon <tonflores99@gmail.com>
2025-09-07 21:01:27 +0800

    Completed Variation1

--- ADT_LIST/Linked_List/Variation1.c
+++ ADT_LIST/Linked_List/Variation1.c
@@ -33,11 +33,14 @@ int main(){
     L = initialize(L);
 
     printf("Insert First\n");
-    L = insertFirst(L, 44);
-    L = insertFirst(L, 33);
-    L = insertFirst(L, 22);
     L = insertFirst(L, 11);
-    // sortList(L);
+    L = insertFirst(L, 44);
+    L = insertFirst(L, 22);
+    L = insertFirst(L, 33);
+    display(L);
+
+    printf("Sorted List\n");
+    L = sortList(L);
     display(L);
 
     printf("Insert Last\n");
@@ -56,43 +59,61 @@ int main(){
     L = deleteStart(L);
     display(L);
 
-    // printf("Delete Last\n");
-    // deleteLast(L);
-    // display(L);
+    printf("Delete Last\n");
+    L = deleteLast(L);
+    display(L);
 
-    // printf("Delete Position\n");
-    // deletePos(L, 2);
-    // display(L);
+    printf("Delete Position\n");
+    L = deletePos(L, 2);
+    display(L);
 
-    // printf("Retrieve Data\n");
-    // int res = retrieve(L, 0);
-    // if(res > 0){
-    //     printf("The data retrieve from that position is %d.\n", res);
-    //     display(L);
-    // }else{
-    //     display(L);
-    // }
+    printf("Retrieve Data\n");
+    int res = retrieve(L, 2);
+    if(res > 0){
+        printf("The data retrieve from that position is %d.\n", res);
+        display(L);
+    }else{
+        display(L);
+    }
 
-    // printf("Locate Data\n");
-    // int got = locate(L, 33);
-    // if(got > 0){
-    //     printf("The data is located in position %d.\n", got);
-    //     display(L);
-    // }else{
-    //     printf("The data did not exist in the list.\n");
-    //     display(L);
-    // }
+    printf("Locate Data\n");
+    int got = locate(L, 11);
+    if(got >= 0){
+        printf("The data is located in position %d.\n", got);
+        display(L);
+    }else{
+        printf("The data did not exist in the list.\n");
+        display(L);
+    }
 
     L = empty(L);
     return 0;
 }
 
 int locate(List list, int data){
+    int got = -1;
 
+    Node *trav = list.head;
+    for(int i = 0; trav != NULL; trav = trav->next, i++){
+        if(trav->data == data){
+            got = i;
+        }
+    }
+
+    return got >= 0 ? got : - 1;
 }
 
 int retrieve(List list, int index){
-
+    int got = -1;
+    if(index >= list.count){
+        printf("The index should be less than or equal to count.\n");
+    }else{
+        Node *trav = list.head;
+        for(int i = 0; i < index + 1; trav = trav->next, i++){
+            got = trav->data;
+        }
+    }
+    return got > 0 ? got : - 1;
 }
 
 List empty(List list){
@@ -109,15 +130,52 @@ List empty(List list){
 }
 
 List sortList(List list){
-
+    for(Node *tempI = list.head; tempI->next != NULL; tempI = tempI->next){
+        for(Node *tempJ = tempI->next; tempJ != NULL; tempJ = tempJ->next){
+            if(tempI->data > tempJ->data){
+                int temp = tempI->data;
+                tempI->data = tempJ->data;
+                tempJ->data = temp;
+            }
+        }
+    }
+    return list;
 }
 
 List deletePos(List list, int index){
+    if(index > list.count){
+        printf("The index should be less than or equal to count.\n");
+    }else if(index == 0){
+        list = deleteStart(list);
+    }else if(index == list.count){
+        list = deleteLast(list);
+    }else{
+        Node *trav = list.head, *curr = trav;
 
+        trav = trav->next;
+        for(int i = 0; i < index - 1; trav = trav->next, curr = curr->next, i++){}
+        Node *prev = curr;
+        trav = trav->next;
+        free(prev);
+        curr->next = trav;
+
+        list.count--;
+    }
+    return list;
 }
 
 List deleteLast(List list){
-    
+    Node *trav, *curr;
+
+    for(trav = list.head; trav->next != NULL; trav = trav->next){
+        curr = trav;
+    }
+    trav = curr->next;
+    free(trav);
+    curr->next = NULL;
+    list.count--;
+
+    return list;
 }
 
 List deleteStart(List list){
