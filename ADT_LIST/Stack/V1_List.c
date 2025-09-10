a23b78590896b3e2db2955941fda35847c2ca2ab
NemesisTon <tonflores99@gmail.com>
2025-09-10 20:30:59 +0800

    finished stack linked list dunno if its correct

--- ADT_LIST/Stack/V1_List.c
+++ ADT_LIST/Stack/V1_List.c
@@ -39,16 +39,72 @@ int main(){
     pop(L);
     display(*L);
 
+    printf("Peek Stack List\n");
+    int got = peek(L);
+    if(got > 0){
+        printf("The top most list is %d.\n", got);    
+    }
+    display(*L);
+
+    printf("Empty Stack List\n");
+    int empty = isEmpty(L);
+    if(empty == 1){
+        printf("Is it empty? Yes.\n");    
+    }else{
+        printf("Is it empty? No.\n");
+    }
+    display(*L);
+
+    printf("Full Stack List\n");
+    int full = isFull(L);
+    if(full == 1){
+        printf("Is it full? Yes.\n");    
+    }else{
+        printf("Is it full? No.\n");    
+    }
+    display(*L);
     return 0;
 }
 
+bool isFull(List *s){
+    if(s->count == MAX){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool isEmpty(List *s){
+    if(s->head == NULL){
+        return true;
+    }else{
+        return false;
+    }
+}
+
 int peek(List *s){
+    if(s->head == NULL){
+        printf("The list is empty.\n");
+        return -1;
+    }
+    int got = - 1;
     
+    Node *trav = s->head;
+    for(; trav->next != NULL; trav = trav->next){}
+    got = trav->data;
+
+    return got > 0 ? got : - 1;
 }
 
 void pop(List *s){
     if(s->count == MAX){
         printf("The linked list is full.\n");
+    }else if(s->count == 1){
+        Node *trav = s->head;
+        free(trav);
+        s->head = NULL;
+        s->count--;
+        printf("The list is empty.\n");
     }else{
         Node *trav = s->head, *prev = trav;
         trav = trav->next;
@@ -93,14 +149,14 @@ void display(List s){
         strcat(buffer, temp);
         trav = trav->next;
     }
-    printf("%-50s Count: %d\n\n", buffer, s.count);
+    printf("%-50s Count: %d\n\n", buffer, s.count + 1);
 }
 
 List* initialize(){
     List *list = (List *)malloc(sizeof(List));
     
     list->head = NULL;
-    list->count = 0;
+    list->count = -1;
 
     return list;
 }
